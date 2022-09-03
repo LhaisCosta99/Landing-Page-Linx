c33a8859630638660536b5de58701d1a05d5273f
Lhais <lhaisferreirada@gmail.com>
2022-09-03 16:45:34 -0300

    aplicada responsividade header

--- TODO9/src/components/Header/Header.jsx
+++ TODO9/src/components/Header/Header.jsx
@@ -9,10 +9,14 @@ export default function Header(){
             <h1>especial para você</h1>
             <p>todos os produtos desta lista foram selecionadas a partir da sua navegação. Aproveite!</p>
             <div>
-                <Button texto="Conheça a Linx" borda={"none"}/>
-                <Button texto="Ajude o algorítimo" borda={"none"}/>
-                <Button texto="Seus produtos" borda={"none"}/>
-                <Button texto="Compartilhe" borda={"none"}/>
+                <div>
+                    <Button texto="Conheça a Linx" borda={"none"} classe={"header-button"} />
+                    <Button texto="Ajude o algorítimo" borda={"none"} classe={"header-button"} />
+                </div>
+                <div>
+                    <Button texto="Seus produtos" borda={"none"} classe={"header-button"} />
+                    <Button texto="Compartilhe" borda={"none"} classe={"header-button"} />
+                </div>
             </div>
             <img src={ triangule } alt="" />
         </header>
